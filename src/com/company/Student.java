334ce8e731968ba1caf285e4bf53d7850c4d8a53
Joey <Joey.Uriarte@saintandrews.net>
2020-10-06 08:08:25 -0400

    Initial commit

---
+++ src/com/company/Student.java
@@ -0,0 +1,68 @@
+package com.company;
+
+public class Student {
+
+
+    public static int mrUStairs(int moveForward, int totalNumOfSteps) {
+    /*
+    Mr. U is working on his fitness...fictionally.
+
+    He is trying to calculate how many steps he will need to take in
+    order to reach the top. Let moveForward be the number of "steps" he can leap in a single bound, while totalNumOfSteps
+    is the number of total steps....duh?
+
+    Once he has reached, or passed, the total number of steps then this method return how times he had to move forward.
+     */
+
+    return 0;
+    }
+
+
+    public boolean guessingGame(int numOfTries, int minNumber, int MaxNumber) {
+      /*
+      What is it about programming problems REALLY liking guessing games, am I right? The goal for this one is simple.
+      When the method is called, the user has a chance to guess the number that will randomly be generated. numOfTries
+      indicates how many attempts they get to guess before the method returns false. IF they guess correctly though,
+      then the method will return true. minNumber and maxNumber is the range that the random numbers will be in, inclusive.
+       */
+        return false;
+    }
+
+    public void annoy(int numOfTimes, String whatToSay) {
+        /*
+        Pretty straight forward - you are going to complete a method that is, quite literally, meant to annoy someone.
+        numOfTimes is the number of times that this method should call System.out.println. What goes inside? Why you guessed
+        it you smart scamp, it's going to be whatToSay.
+         */
+    }
+
+
+    public int addItUp(int numToAddUpTo) {
+        /*
+        This method will take in an int, then add up all numbers before, and including it, to show the total value. So for example
+        if I call addItUp(3), then this method would return 6     (1+2+3)
+         */
+        return 0;
+    }
+
+    public void afraidOfNumbers(int numToStopAt, boolean useModulo) {
+        /*
+        So between us, Mr. U absolutely hates odd numbers. His life is one of tragedy and sorrow, all due to
+        even numbers. How? Man, I don't know...I'm just a guy trying to make programming problems that don't make you
+        want to fall asleep.
+
+        Okay fine, cause 7 8 my entire family. Get it?
+
+        .....
+
+        Anyways - this method works in one of two ways.
+
+        IF the boolean is set to true, then it should loop from 0 to numToStopAt and when, and only when, the number
+        is confirmed as even should it be output through System.out.println.
+
+        IF the boolean is set to false, then you should skip over the odd numbers within the for loop itself.
+
+        Boom, I just made you do two different for loop problems in one. You're welcome.
+         */
+    }
+}
